1603adc5dc47bc3d2c9c1eb383dc86963f023d7e
KhaledBaki <khaledbaki2018@gmail.com>
2026-09-14 10:01:04 -0400

    Added Singly linked list template

---
+++ Linked Lists/Singly Linked List/SinglyLinkedList.java
@@ -0,0 +1,23 @@
+public class SinglyLinkedList {
+    private SinglyLinkedListNode head;
+
+    private SinglyLinkedListNode tail;
+
+    head = tail = null
+
+    public SinglyLinkedList() {
+
+    }
+    // Add element at index
+
+    // Delete element at index
+
+    // Add first
+
+    // Delete first
+
+    // Add last
+
+    // delete last
+
+}
